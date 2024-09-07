4ba595d1e9295134790a0be9b195c57180f4863a
emkam5 <wertysedt@gmail.com>
2024-09-07 10:03:08 +0300

    proj1

---
+++ assets/js/alert.js
@@ -0,0 +1,7 @@
+'use strict';
+alert('Я Java Scripy');
+let fama = prompt('введите фамилию');
+let name = prompt('введите имя');
+let otec = prompt('введите отчество');
+let isTrue = confirm(`верна ли ${fama} ${name} ${otec}`);
+alert(isTrue)
